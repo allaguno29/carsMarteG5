220b3ddd782ee1786f1d0fe4ae57bc7aaf8583c8
Josue Vera <josvecastro@espol.com>
2021-09-01 18:48:52 -0500

    metodo registrar camioneta ne la clase camioneta

--- src/main/java/ec/edu/espol/model/Camioneta.java
+++ src/main/java/ec/edu/espol/model/Camioneta.java
@@ -8,6 +8,12 @@ package ec.edu.espol.model;
 import ec.edu.espol.util.Util;
 import java.io.Serializable;
 import java.util.Scanner;
+import javafx.geometry.Pos;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -88,5 +94,60 @@ public class Camioneta extends Vehiculo implements Serializable
         Vehiculo camioneta= new Camioneta(vidrios,placa,marca,modelo,motor,año,recorrido,color,combustible,precio,transmision,traccion);
         camioneta.saveFile("vehiculos.ser"); 
         camioneta.saveFile("camionetas.ser");
+    }
+    public static void registrarCamioneta(ScrollPane sp){
+        VBox vbox= new VBox();
+        vbox.setAlignment(Pos.CENTER);
+        sp.setContent(vbox);
+        HBox hbox1 = new HBox();
+        Text marcat= new Text("Marca:");
+        TextField marca = new TextField();
+        hbox1.getChildren().add(marcat);
+        hbox1.getChildren().add(marcat);
+        HBox hbox2 = new HBox();
+        Text modelot= new Text("Modelo:");
+        TextField modelo = new TextField();
+        hbox2.getChildren().add(modelot);
+        hbox2.getChildren().add(modelo);
+        HBox hbox3 = new HBox();
+        Text motort= new Text("Tipo de motor:");
+        TextField motor = new TextField();
+        hbox3.getChildren().add(motort);
+        hbox3.getChildren().add(motor);
+        HBox hbox4 = new HBox();
+        Text placat= new Text("Año del vehiculo:");
+        TextField año = new TextField();
+        hbox4.getChildren().add(placat);
+        hbox4.getChildren().add(año);
+        HBox hbox5 = new HBox();
+        Text kilometrajet= new Text("kilometraje:");
+        TextField kilometraje = new TextField();
+        hbox5.getChildren().add(kilometrajet);
+        hbox5.getChildren().add(kilometraje);
+        HBox hbox6 = new HBox();
+        Text combustiblet= new Text("Tipo de combustible:");
+        TextField combustible = new TextField();
+        hbox6.getChildren().add(combustiblet);
+        hbox6.getChildren().add(combustible);
+        HBox hbox7 = new HBox();
+        Text colort= new Text("Color dle vehiculo:");
+        TextField color = new TextField();
+        hbox7.getChildren().add(colort);
+        hbox7.getChildren().add(color);
+        HBox hbox8 = new HBox();
+        Text preciot= new Text("Precio:");
+        TextField precio = new TextField();
+        vbox.getChildren().add(preciot);
+        vbox.getChildren().add(precio);
+        vbox.getChildren().add(hbox1);
+        vbox.getChildren().add(hbox2);
+        vbox.getChildren().add(hbox3);
+        vbox.getChildren().add(hbox4);
+        vbox.getChildren().add(hbox5);
+        vbox.getChildren().add(hbox6);
+        vbox.getChildren().add(hbox7);
+        vbox.getChildren().add(hbox8);
+        double añov= Double.parseDouble(año.getText());
+        
     }
 }
